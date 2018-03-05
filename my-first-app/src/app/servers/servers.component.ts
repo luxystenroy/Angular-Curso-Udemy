5469aec7a26b1cc779eb78660b05cd511dd9bd87
Luis Gónzalez <luisgonzalez@Luiss-MBP.iptv.microsoft.com>
2018-03-05 20:47:39 -0300

    selector by attr and class

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  //selector: '[app-servers]', by attr
+  selector: '.app-servers',
   template: `<app-server></app-server>
   <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
