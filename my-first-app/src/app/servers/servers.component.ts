4efa9bffe43dd73538464fbfa7832ad0c4bda074
Luis Gónzalez <luisgonzalez@Luiss-MBP.iptv.microsoft.com>
2018-03-05 21:43:19 -0300

    databing

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,14 +2,23 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   //selector: '[app-servers]', by attr
-  selector: '.app-servers',
-  template: `<app-server></app-server>
-  <app-server></app-server>`,
+  selector: 'app-servers',
+  /*template: `<app-server></app-server>
+  <app-server></app-server>`,*/
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+
+  constructor() {
+
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },2000)
+
+  }
 
   ngOnInit() {
   }
